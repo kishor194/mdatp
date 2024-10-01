7c9a626f7f4dd95e091b0e84bdf3802945cd554c
kishor194 <77243373+kishor194@users.noreply.github.com>
2024-10-01 12:42:18 +0530

    first

---
+++ mdatp.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Get the hostname of the VM
+VM_HOSTNAME=$(hostname)
+
+# Print the hostname
+echo "The hostname of this VM is: $VM_HOSTNAME"
+
+# Navigate to /root directory
+cd /root
+
+# Download the mde_installer.sh script
+curl -O https://raw.githubusercontent.com/microsoft/mdatp-xplat/refs/heads/master/linux/installation/mde_installer.sh
+
+curl -O https://raw.githubusercontent.com/kishor194/mdatp/refs/heads/main/onboarding.py
+
+# Make the installer executable
+chmod u+x mde_installer.sh
+
+# Run the installer with the hostname used as the tag
+sudo /root/mde_installer.sh --install --channel prod --onboard /root/onboarding.py --tag "GROUP $VM_HOSTNAME" --min_req -y
+
+# Enable real-time protection
+sudo mdatp config real-time-protection --value enabled
+
+# Enable behavior monitoring
+sudo mdatp config behavior_monitoring --value enabled
+
+# Check the health of Microsoft Defender
+sudo mdatp health
